cbbb887efbea49a8ba6cb6c6c35a417a7d83a343
Nattapon <nattaponmax5@gmail.com>
2025-11-17 17:17:32 +0700

    lab101

---
+++ LabSheet01/src/Lab101.java
@@ -0,0 +1,8 @@
+public class Lab101 {
+    public static void main(String[] args) {
+        System.out.println("I am Nattaon SUKPITAK");
+        //System.out.println("\"INT-108\" Objected Programming");
+        System.out.println("\tThis is my first time learning Java");
+        System.out.println("I hope to get grade \"A\"");
+    }
+}
